478af6472f5e4d72f0d555f11677e008d5e7adb6
buntafujikawa <bunta.fujikawa@gmail.com>
2019-12-30 20:28:11 +0900

    complete nodecli

--- nodecli/main.js
+++ nodecli/main.js
@@ -20,7 +20,7 @@ https://nodejs.org/dist/latest-v12.x/docs/api/process.html#process_process
 
 const program = require("commander");
 const fs = require("fs");
-const marked = require("marked");
+const md2html = require("./md2html");
 
 // gfmオプションを定義
 program.option("--gfm", "GFMを有効にする");
@@ -45,8 +45,6 @@ fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     process.exit(1);
     return;
   }
-  const html = marked(file, {
-    gfm: cliOptions.gfm,
-  });
+  const html = md2html(file, cliOptions);
   console.log(html);
 });

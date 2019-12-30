3936c6896a2f7d92953eea84f2505cc7543d921a
buntafujikawa <bunta.fujikawa@gmail.com>
2019-12-30 19:09:24 +0900

    complete ajax app

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -5,29 +5,38 @@ Fetch APIはHTTP通信を行いリソースを取得するためのAPIです。F
 Fetch APIは同じくHTTP通信を扱うXMLHttpRequestと似たAPIですが、より強力で柔軟な操作が可能です。
  */
 
-function main() {
-  /*
-  Promiseチェーンを使って処理を分割する利点は、同期処理と非同期処理を区別せずに連鎖できることです
-  一般に、同期的に書かれた処理を後から非同期処理へと変更することは、全体を書き換える必要があるため難しいです。
-  そのため、最初から処理を分けておき、処理をthenを使ってつなぐことで、変更に強いコードを書くことができます。
-   */
-  fetchUserInfo("buntafujikawa")
-  .then((userInfo) => createView(userInfo))
-  .then((view) => displayView(view))
-  .catch((error) => {
+/*
+Promiseチェーンを使って処理を分割する利点は、同期処理と非同期処理を区別せずに連鎖できることです
+一般に、同期的に書かれた処理を後から非同期処理へと変更することは、全体を書き換える必要があるため難しいです。
+そのため、最初から処理を分けておき、処理をthenを使ってつなぐことで、変更に強いコードを書くことができます。
+ */
+async function main() {
+  try {
+    const userId = getUserId();
+    const userInfo = await fetchUserInfo(userId);
+    const view = createView(userInfo);
+    displayView(view);
+  } catch (error) {
     console.error(`エラーが発生しました (${error})`);
-  });
+  }
 }
 
 function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
   .then(response => {
+    // console.log(response);
+    // console.log(response.json());
     if (!response.ok) {
       new Promise.reject(new Error(`${response.status}: ${response.statusText}`));
     } else {
-      return response.json;
+      return response.json();
     }
   });
+}
+
+function getUserId() {
+  const value = document.getElementById("userId").value;
+  return encodeURIComponent(value);
 }
 
 function createView(userInfo) {
